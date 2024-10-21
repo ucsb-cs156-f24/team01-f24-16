dec2cfc124fc697396d3dbedfe0fa8c993f3d048
Kelly Li <kellyli@ucsb.edu>
2024-10-21 16:40:40 -0700

    added ucsbdiningcommonsmenuitem entity repository and db migration file

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
@@ -0,0 +1,20 @@
+package main.java.edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The UCSBDiningCommonsMenuItemRepository is a repository for UCSBDiningCommonsMenuItem entities.
+ */
+
+@Repository
+public class UCSBDiningCommonsMenuItemRepository extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {
+  // /**
+  //  * This method returns all UCSBDate entities with a given quarterYYYYQ.
+  //  * @param quarterYYYYQ quarter in the format YYYYQ (e.g. 20241 for Winter 2024, 20242 for Spring 2024, 20243 for Summer 2024, 20244 for Fall 2024)
+  //  * @return all UCSBDate entities with a given quarterYYYYQ
+  //  */
+  // Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);
+}
